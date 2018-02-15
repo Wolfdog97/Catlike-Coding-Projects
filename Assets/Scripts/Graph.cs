b991ef1d1a305fe1d67a663f9eaa91d37834a358
Unknown <adh416@nyu.edu>
2018-02-14 19:11:21 -0500

    Creating prefabs and scripts

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform pointPrefab;
+
+    private void Awake()
+    {
+        Transform point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right;
+    }
+}
