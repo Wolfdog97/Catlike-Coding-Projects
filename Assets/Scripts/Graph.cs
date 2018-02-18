92ec37135a6748ab30f06fdf87fe8b6e84b0dbcf
Unknown <adh416@nyu.edu>
2018-02-18 14:37:18 -0500

    Finished building a graph
    Finished building a graph tutorial and moved on to the mathematical surfaces tutorial on the same scene

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,9 +4,69 @@ public class Graph : MonoBehaviour
 {
     public Transform pointPrefab;
 
+    Transform[] points;
+
+    [Range(10, 100)]
+    public int resolution = 10;
+
+    public GraphFunctionName function;
+
+    static GraphFunction[] functions =
+    {
+        SineFunction, MultiSineFunction
+    };
+
     private void Awake()
     {
-        Transform point = Instantiate(pointPrefab);
-        point.localPosition = Vector3.right;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position;
+        position.y = 0f;
+        position.z = 0f;
+
+        points = new Transform[resolution * resolution];
+
+       for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if(x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+            Transform point = Instantiate(pointPrefab);
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
+            point.localPosition = position; 
+            point.localScale = scale;
+            point.SetParent(transform, false);
+
+            points[i] = point;
+        }
+    }
+    private void Update()
+    {
+        float t = Time.time;
+        GraphFunction f = functions[(int)function];
+       
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = f(position.x, position.z, t);
+            point.localPosition = position;
+        }
+    }
+
+    static float SineFunction (float x, float z, float t)
+    {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+
+    static float MultiSineFunction (float x, float z, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f * Mathf.PI * (x * 2f + t)) / 2f;
+        y *= 2f / 3f;
+        return y;
     }
 }
